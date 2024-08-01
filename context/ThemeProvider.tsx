65fb83a7c9afaf26da1d84d6eb4b8b77f2368f3b
Azaan saifi <saifiazaan1@gmail.com>
2024-08-01 09:21:29 +0530

    create navbar

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -13,7 +13,11 @@ export function ThemeProvider({ children }: { children: React.ReactNode }) {
   const [mode, setMode] = useState("");
 
   const handleThemeChange = () => {
-    if (mode === "dark") {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
       setMode("dark");
       document.documentElement.classList.add("dark");
     } else {
@@ -24,7 +28,7 @@ export function ThemeProvider({ children }: { children: React.ReactNode }) {
 
   useEffect(() => {
     handleThemeChange();
-  }, []);
+  }, [mode]);
 
   return (
     <ThemeContext.Provider value={{ mode, setMode }}>
