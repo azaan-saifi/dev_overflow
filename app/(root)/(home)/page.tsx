c693c9682daee1e08fd9b2b0f280183ff74ffdcd
Azaan saifi <saifiazaan1@gmail.com>
2024-08-05 10:49:28 +0530

    implement asking questions functionality and displaying them on the homepage by fetching data.

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -5,65 +5,13 @@ import Filter from "@/components/shared/search/Filter";
 import { LocalSearchbar } from "@/components/shared/search/LocalSearchbar";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filter";
+import { getQuestions } from "@/lib/actions/question.action";
 import Link from "next/link";
 import React from "react";
 
-const questions = [
-  {
-    _id: "1",
-    title:
-      "Best practices for data fetching in a Next.js application with Server-Side Rendering (SSR)?",
-    tags: [
-      { _id: "1", name: "Nextjs" },
-      { _id: "2", name: "Reactjs" },
-    ],
-    author: {
-      _id: "1",
-      name: "John Doe",
-      picture: "https://via.placeholder.com/150",
-    },
-    answers: [
-      {
-        _id: "1",
-        content: "You can use getServerSideProps for SSR in Next.js.",
-      },
-      { _id: "2", content: "Another approach is to use SWR library with SSR." },
-    ],
-    views: 120000,
-    upvotes: 10,
-    createdAt: new Date("2024-08-1"),
-  },
-  {
-    _id: "2",
-    title: "Is it only me or the font is bolder than necessary?",
-    tags: [
-      { _id: "3", name: "Css" },
-      { _id: "4", name: "Html" },
-    ],
-    author: {
-      _id: "2",
-      name: "John Doe",
-      picture: "https://via.placeholder.com/150",
-    },
-    answers: [
-      {
-        _id: "1",
-        content:
-          "You might want to check the font-weight property in your CSS.",
-      },
-      {
-        _id: "2",
-        content:
-          "Also, make sure there are no conflicting styles from parent elements.",
-      },
-    ],
-    views: 120,
-    upvotes: 10,
-    createdAt: new Date("2024-03-30"),
-  },
-];
+const Home = async () => {
+  const result = await getQuestions({});
 
-const Home = () => {
   return (
     <>
       <div className="-mt-2 flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
@@ -92,8 +40,8 @@ const Home = () => {
       <HomeFilters />
 
       <div className="mt-10 flex w-full flex-col gap-6">
-        {questions.length > 0 ? (
-          questions.map((question) => (
+        {result.questions.length > 0 ? (
+          result.questions.map((question) => (
             <QuestionCard
               key={question._id}
               _id={question._id}
--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,7 +1,26 @@
+import Question from "@/components/form/Question";
+import { getUserById } from "@/lib/actions/user.action";
+// import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const AskQestion = () => {
-  return <div>Ask-question</div>;
+const AskQestion = async () => {
+  // const { userId } = auth()
+
+  const userId = "123456";
+
+  if (!userId) redirect("/sign-in");
+
+  const mongoUser = await getUserById({ userId });
+
+  return (
+    <div>
+      <h1 className="h1-bold text-dark200_light800">Ask a question</h1>
+      <div className="mt-6">
+        <Question mongoUserId={JSON.stringify(mongoUser._id)} />
+      </div>
+    </div>
+  );
 };
 
 export default AskQestion;
