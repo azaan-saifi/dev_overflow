6bbcb7dcd693e4117ba50df8d1de0e5556540c53
Azaan saifi <saifiazaan1@gmail.com>
2024-08-05 12:02:51 +0530

    add webhooks

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,8 +2,16 @@
 
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
+import {
+  CreateUserParams,
+  DeleteUserParams,
+  GetUserByIdParams,
+  UpdateUserParams,
+} from "./shared.types";
+import { revalidatePath } from "next/cache";
+import Question from "@/database/question.model";
 
-export async function getUserById(params: any) {
+export async function getUserById(params: GetUserByIdParams) {
   try {
     connectToDatabase();
 
@@ -13,6 +21,61 @@ export async function getUserById(params: any) {
     return user;
   } catch (error) {
     console.log("Error fetching user by ID:", error);
+    throw error;
+  }
+}
+
+export async function createUser(userData: CreateUserParams) {
+  try {
+    connectToDatabase();
+
+    const newUser = await User.create(userData);
+
+    return newUser;
+  } catch (error) {
+    console.log("Error while creating the user:", error);
+    throw error;
+  }
+}
+
+export async function updateUser(params: UpdateUserParams) {
+  try {
+    connectToDatabase();
+
+    const { clerkId, updateData, path } = params;
+    await User.findOneAndUpdate({ clerkId }, updateData, { new: true });
+    revalidatePath(path);
+  } catch (error) {
+    console.log("Error while creating the user:", error);
+    throw error;
+  }
+}
+
+export async function deleteUser(params: DeleteUserParams) {
+  try {
+    connectToDatabase();
+
+    const { clerkId } = params;
+
+    const user = await User.findOneAndDelete({ clerkId });
+    if (!user) {
+      throw new Error("User not found!");
+    }
+
+    // Delete user from our database
+    // and their questions, answers, comments etc.
+
+    // const userQuestionIds = await Question.find({ author: user._id }).distinct(
+    //   "_id"
+    // );
+    // Deleting the questions
+    await Question.deleteMany({ author: user._id });
+
+    // TODO: Delete answers, comments
+
+    return user;
+  } catch (error) {
+    console.log("Error while creating the user:", error);
     throw error;
   }
 }
